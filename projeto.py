9308ecef5ccf744943c1bbd5f0978a58559debcb
Thiago <thiagogobb@outlook.com>
2023-06-13 11:52:58 -0300

    chore search and insert in dictionary

--- projeto.py
+++ projeto.py
@@ -1,14 +1,38 @@
+def cadastro(cadastros):
+    while True:
+        status = "em funcionamento"
+        codigo = input("Código: ")
+        nome = input("Nome: ")
+        cadastros[codigo] = {"nome": nome, "status": status}
+        print("Cadastro realizado!",codigo, ":", [cadastros[codigo]["nome"], cadastros[codigo]["status"]])
+        flag = input("Deseja realizar novo cadastro (s/n)? ")
+        if flag == "n":
+            break
+
+def consulta(cadastros):
+    busca = input("Busca: ")
+    encontrou = False
+
+    for codigo, info in cadastros.items():
+        if busca in [codigo, info["nome"], info["status"]]:
+            print(codigo, ":", [info["nome"], info["status"]])
+            encontrou = True
+
+    if not encontrou:
+        print("Nenhum resultado encontrado.")
+
+
 def main():
+    cadastros = {}
     print("Menu")
     escolha = input("1- Cadastro\n2- Consulta\n3- Reparo\n4- Sair\nDigite uma opção: ")
     while(escolha!="4"):
 
         if(escolha=="1"):
-            print("Cadastro")
-            #cadastro()
+            cadastro(cadastros)
 
         elif(escolha=="2"):
-            print("Consulta")
+            consulta(cadastros)
             #consulta()
 
         elif(escolha=="3"):
@@ -22,3 +46,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
