9082b68e2b47f2c3c2b85af9dd573f2c190c5b38
Thiago <thiagogobb@outlook.com>
2023-06-13 10:11:26 -0300

    chore menu setup

--- projeto.py
+++ projeto.py
@@ -1,1 +1,24 @@
-print("Hello, world!")
+def main():
+    print("Menu")
+    escolha = input("1- Cadastro\n2- Consulta\n3- Reparo\n4- Sair\nDigite uma opção: ")
+    while(escolha!="4"):
+
+        if(escolha=="1"):
+            print("Cadastro")
+            #cadastro()
+
+        elif(escolha=="2"):
+            print("Consulta")
+            #consulta()
+
+        elif(escolha=="3"):
+            print("Reparo")
+            #reparo()
+
+        else:
+            print("Opção inválida")
+
+        escolha = input("1- Cadastro\n2- Consulta\n3- Reparo\n4- Sair\nDigite uma opção: ")
+
+if __name__ == "__main__":
+    main()
